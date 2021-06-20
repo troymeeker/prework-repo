4c2ec58e1fcf2ad293719522935dc13db750e2c8
troymeeker <troymeeker1@gmail.com>
2021-06-20 11:38:46 -0700

    commit js

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 
-function introHello(name, age){
- console.log('My name is ' + name + ' and I am ' + age + ' years old');
-}
-introHello("Troy", 26);
-
-function calcArea(width, length){
-    console.log( length * width);
-}
-calcArea(4*6);
+const headerOne = document.querySelector('h1')
