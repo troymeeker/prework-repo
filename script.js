b5fed52f149a89b329f5e32ffeb5a1cb408c3050
troymeeker <troymeeker1@gmail.com>
2021-06-19 23:55:14 -0700

    adding js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-function introHello(name){
- console.log('My name is ' + name + 'and I am ' + age + ' years old');
+function introHello(name, age){
+ console.log('My name is ' + name + ' and I am ' + age + ' years old');
 }
 introHello("Troy", 26);
+
+function calcArea(width, length){
+    console.log( length * width);
+}
+calcArea;
