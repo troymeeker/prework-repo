09f2ad4cb9cf2c71649bdf29156108eb9576830e
troymeeker <troymeeker1@gmail.com>
2021-06-20 12:50:02 -0700

    js changes

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const headerOne = document.querySelector('h5')
 
 headerOne.style.color = 'black';
 headerOne.innerText = "Follow us to see more fun routes in Oregon!";
+
+const itemFive = document.getElementById('item-five');
+ itemFive.innerText = "Mt Jefferson Wilderness - Sisters/Bend"
+
+const itemTwo = document.getElementById('item-two');
+itemTwo.innerText = "Forest Park/ Wildwood Trail - Portland"
